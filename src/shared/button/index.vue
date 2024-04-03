7d1709c241562062b1ba83775a81d586892ab60e
Yurii Rybachok <drummer2009@ukr.net>
2024-04-03 12:07:37 +0300

    create button component

---
+++ src/shared/button/index.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+interface Props {
+  leftIcon?: HTMLElement
+  rightIcon?: HTMLElement
+  color?: 'primary' | 'secondary' | 'grayscale' | 'error'
+  decoration?: 'default' | 'outline' | 'none'
+  size?: 'L' | 'M' | 'S'
+  disabled?: boolean
+}
+
+const props = defineProps<Props>()
+
+const {
+  leftIcon = null,
+  rightIcon = null,
+  color = 'primary',
+  decoration = 'default',
+  size = 'M',
+  disabled = false
+} = props
+</script>
+
+<template>
+  <button></button>
+</template>
+
+<style scoped></style>
--- src/widgets/header/index.vue
+++ src/widgets/header/index.vue
@@ -4,8 +4,24 @@ import Logo from '@/shared/logo/index.vue'
 </script>
 <template>
   <header class="header">
-    <Container> <Logo orientation="horizontal" bgColor="white" colorFull withText /> </Container>
+    <Container class="header__container">
+      <Logo orientation="horizontal" bgColor="white" colorFull withText />
+    </Container>
   </header>
 </template>
 
-<style scoped></style>
+<style scoped>
+.header__container {
+  display: flex;
+  align-items: center;
+  height: 72px;
+}
+
+.header {
+  position: sticky;
+  top: 0;
+  left: 0;
+  background: var(--surface);
+  box-shadow: var(--shadow-default-s);
+}
+</style>
