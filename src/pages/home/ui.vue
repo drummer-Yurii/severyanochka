dd8def01a44550158c50fb3f8ccdc080a8bb0a8b
Yurii Rybachok <drummer2009@ukr.net>
2024-04-08 17:35:56 +0300

    create product card / badge / like

--- src/pages/home/ui.vue
+++ src/pages/home/ui.vue
@@ -1,10 +1,28 @@
 <script setup lang="ts">
+import { reactive } from 'vue'
 import { Header } from '@/widgets/header'
 import { BottomTabNavigator } from '@/widgets/bottom-tab-navigator'
 import { Footer } from '@/widgets/footer'
+import { Cards, type CardsProps } from '@/widgets/cards'
 import { MainCarousel } from '@/features/main-carousel'
 import { Content } from '@/shared/content'
+import { Container } from '@/shared/container'
+import product1PNG from '@/assets/product-1.png'
 
+const saleCards = reactive<CardsProps>({
+  data: {
+    title: 'Акции',
+    listLinkText: 'Все акции',
+    listLinkHref: '/',
+    items: [
+      {
+        img: product1PNG,
+        name: 'Г/Ц Блинчики с мясом вес',
+        sale: 50
+      }
+    ]
+  }
+})
 </script>
 
 <template>
@@ -12,6 +30,9 @@ import { Content } from '@/shared/content'
   <BottomTabNavigator />
   <Content>
     <MainCarousel />
+    <Container :style="{ margin: '80px auto' }">
+      <Cards :data="saleCards.data" />
+    </Container>
   </Content>
   <Footer />
 </template>
